aae98d44a2e1a4c7ff8af98628505d1043bd1522
Thiana <thianacantunes@gmail.com>
2023-05-10 20:16:12 -0300

    Versão com o CRUD dos Jogos pronto

--- src/controllers/jogos.controllers.js
+++ src/controllers/jogos.controllers.js
@@ -1,7 +1,29 @@
+import { db }  from "../database/database.connection.js"
+
 export async function getJogos(req, res){
-    res.send("getJogos")
+
+    try{
+        const jogos = await db.query(`SELECT * FROM games;`)
+        res.send(jogos.rows).status(200)
+    } catch (err){
+        res.status(500).send(err.message)
+    }
 }
 
 export async function inserirJogo(req, res){
-    res.send("inserirJogo")
+
+    const {name, image, stockTotal, pricePerDay} = req.body
+
+    const jogoExistente = await db.query (`SELECT * FROM games WHERE name =$1`, [name])
+
+    if(!jogoExistente.rows) return res.sendStatus(409)
+
+    try {
+        await db.query(`INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4);`, 
+        [name, image, stockTotal, pricePerDay])
+        return res.sendStatus(201)
+    } catch (err){
+        res.status(500).send(err.message)
+    }
+    
 }
