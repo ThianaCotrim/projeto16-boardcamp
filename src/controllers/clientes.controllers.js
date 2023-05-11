1bc7c95ad20f7922adb274dfb896b6c42c621c8d
Thiana <thianacantunes@gmail.com>
2023-05-11 09:55:38 -0300

    Versão com o requisito Clientes implementado

--- src/controllers/clientes.controllers.js
+++ src/controllers/clientes.controllers.js
@@ -1,15 +1,68 @@
+import { db } from "../database/database.connection.js"
+
 export async function getClientes(req, res){
-    res.send("getClientes")
+
+    try{
+        const clientes = await db.query(`SELECT * FROM customers`)
+        res.send(clientes.rows)
+    } catch(err){
+        res.send(err.message)
+    }
 }
 
 export async function getClientesById(req, res){
-    res.send("getClientesById")
+    const {id} = req.params
+
+    try{
+        const users = await db.query(`SELECT * FROM customers WHERE id=$1;`, [id])
+
+        if(users.rows.length === 0) return res.status(404).send("Esse id não existe")
+        res.send(users.rows[0])
+    } catch(err){
+        return res.sendStatus(err.message)
+    }
 }
 
 export async function inserirClientes(req, res){
-    res.send("inserirClientes")
+    const {name, phone, cpf, birthday} = req.body
+
+    if (name === '') return res.status(400).send("O nome do cliente não pode estar vazio")
+
+    if (!/^\d{11}$/.test(cpf)) return res.status(400).send("O cpf deve conter 11 caracteres")
+
+    if (!/^\d{10,11}$/.test(phone)) return res.status(400).send("O phone deve ter entre 10 e 11 caracteres")
+
+    const cpfExistente = await db.query (`SELECT * FROM customers WHERE cpf = $1;`, [cpf])
+    if (cpfExistente.rows.length) return res.status(409).send("CPF já existente, escolha outro cpf")
+
+    try{
+        await db.query(`INSERT INTO customers (name, phone, cpf, birthday) 
+        VALUES ($1, $2, $3, $4);`,
+        [name, phone, cpf, birthday])
+        return res.sendStatus(201)
+
+    } catch (err){
+        res.status(500).send(err.message)
+    }
 }
 
 export async function editClientesById(req, res){
+    const {id} = req.params
+    const {name, phone, cpf, birthday} = req.body
+
+    try{
+        await db.query(`SELECT * FROM customers WHERE id=$1;`, [id])
+
+        await db.query(`UPDATE customers SET name=$1, phone=$2, cpf=$3, birthday=$4 WHERE id=$5`,
+         [name, phone, cpf, birthday, id])
+        return res.send("Cliente atualizado com sucesso")
+    } catch (err){
+        res.status(500).send("Erro ao atualizar cliente")
+    }
+
+    
+    
+
+
     res.send("editClientesById")
 }
